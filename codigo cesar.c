6018564111fb03423d6f061cdb04c8d64e7818bb
Sergio Mendez <39496193+sergio-mndz@users.noreply.github.com>
2018-05-22 12:04:13 -0500

    Codigo cesar
    Código para la clase de programación

---
+++ codigo cesar.c
@@ -0,0 +1,44 @@
+/*
+ ============================================================================
+ Name        : codigo.c
+ Author      : Sergio
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void encriptar(char cadena[50], int desplazamiento){
+	for(int i = 0; i<strlen(cadena);i++){
+		if((cadena[i]>64 && cadena[i]<91)){
+			if((cadena[i]+desplazamiento)>90){
+				cadena[i] = cadena[i]+desplazamiento-26;
+			} else {
+				cadena[i] = cadena[i]+desplazamiento;
+			}
+		} else if (cadena[i]>96 && cadena[i]<123){
+			if((cadena[i]+desplazamiento)>122){
+				cadena[i] = cadena[i]+desplazamiento-26;
+			} else {
+				cadena[i] = cadena[i]+desplazamiento;
+			}
+		}
+	}
+	printf("%s", cadena);
+}
+
+int main(void) {
+	setvbuf(stdout, NULL, _IONBF, 0);
+	char texto[50];
+	int des;
+
+	printf("Introduce el texto: ");
+	fgets(texto, 50, stdin);
+	printf("Introduce el desplazamiento: ");
+	scanf("%d", &des);
+	encriptar(texto, des);
+}
